183f6f978631c511ae50ca9a7bfe61316b08f421
Gift13-07 <giftchinwendu07@gmail.com>
2022-07-21 13:55:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+console.log("Hello, Welcome to Codeville")
+let school="Shalom Science and Technology Academy";
+console.log(school)
+let giftname= "Gift"+"Chinwendu"
+console.log(giftname)
+let word=2+2
+console.log(word)
+let x=2
+let y=4
+let add=x+y
+let subtract=y-x
+let divide=y/x
+let multiply=x*y
+console.log(add)
+console.log(subtract)
+console.log(divide)
+console.log(multiply)
+let equals=x==y
+console.log(equals)
+let notequals=x!=y
+console.log(notequals)
+let great=x>y
+console.log(great)
+let less=x<y
+console.log(less)
+let win
+console.log(win)
